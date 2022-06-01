dc95ab688ffe1fa78290ac7e762e870dbfce27d7
Nick Garbalau <garbalaunick@protonmail.com>
2022-06-01 10:15:38 +0300

    Connected React and GraphqL

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// GraphQLs
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+// Single GraphQL Endpoint Route
+const URI = 'https://rickandmortyapi.com/graphql';
+
+const client = new ApolloClient({
+  uri: URI,
+  cache: InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrapping whole application in the Apollo provider component */}
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
